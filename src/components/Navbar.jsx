bfd25e8921fca5552a29c6097d58a58408994b58
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-10-28 14:16:36 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const links =[
         ))}
         </ul>
 
-        <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">{nav ? <FaTimes size={20} /> : <FaBars size={20} />}</div>
+        <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">{nav ? <BiCaretLeftCircle size={20} /> : <BiMenuAltLeft size={20} />}</div>
 
         {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
