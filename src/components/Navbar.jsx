a4a1b7219e8e4b46357774e2d1a940055cbf9141
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-10-28 14:23:34 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,22 +12,7 @@ const links =[
         id: 1,
         link: "home",
     },
-    {
-        id: 2,
-        link: "about",
-    },
-    {
-        id: 3,
-        link: "portfolio",
-    },
-    {
-        id: 4,
-        link: "experience",
-    },
-    {
-        id: 5,
-        link: "contact",
-    },
+ 
 ];
 
  return (
