8ec1e486765dc70e30e3220a1adeaea5fc997e94
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-10-28 14:09:49 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const [nav, setNav] = useState(false)
         <nav>
             <ul className='flex flex-col p-14 text-gray-800'>
                 <li className='text-2xl py-4 pb-5 flex text-white'><BiHomeAlt size={30} className='mr-4 text-red-300' />
-                  <a duration="500"/>Home</li>
+                  <a duration="500">Home</a></li>
                 <li className='text-2xl py-4 pb-5 flex text-white'><BiNews size={30} className='mr-4  text-red-300' />Learn More</li>
                 <li className='text-2xl py-4 pb-5 flex text-white'><BiAlignLeft size={30} className='mr-4  text-red-300' />Our Selection</li>
                 <li className='text-2xl py-4 pb-5 flex text-white'><BiCartAlt size={30} className='mr-4  text-red-300' />Make An Order</li>
