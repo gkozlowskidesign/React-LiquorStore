35602150d80f7129e3e3e4d9e39a9e450c087aee
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 01:10:19 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,19 +26,19 @@ const [nav, setNav] = useState(false)
         <nav>
             <ul className='flex flex-col p-14 text-gray-800'>
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiHomeAlt size={30} className='mr-4  text-red-400' />
+                      <BiHomeAlt size={30} className='mr-4  text-white' />
                     <a href="/" className="mt-2 text-red-300 hover:text-white">Home</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiCommentCheck size={30} className='mr-4  text-red-400' />
+                      <BiCommentCheck size={30} className='mr-4  text-white' />
                     <a href="https://linktr.ee/cindyswineandspirits" className="mt-2 text-red-300 hover:text-white">Connect</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiMap size={30} className='mr-4  text-red-400' />
+                      <BiMap size={30} className='mr-4 text-white' />
                     <a href="https://goo.gl/maps/8Lpo34KA7kgbJgTj9" className="mt-2 text-red-300 hover:text-white">Location</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiSupport size={30} className='mr-4  text-red-400' />
+                      <BiSupport size={30} className='mr-4  text-white' />
                     <a href="tel:(860) 399-0007" className="mt-2 text-red-300 hover:text-white">Call Us</a></li>
 
             </ul>
