0f12976c6b74c38b20d8a639467455d7d0cbf716
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 00:28:27 -0500

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -28,36 +28,30 @@ const products = [
   ]
   
 
-  export default function Example() {
-    return (
+export default function Example() {
+return (
       
                     
 <div id="Contact" className='w-full bg-black text-white'>
-        <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto'>
-            <br></br>
-            <div className='pb-2'>
-                <p className='text-4xl font-bold inline border-b-4 border-red-500'>Menu</p>
-                <p className='py-6'>Click on our collectible NFT publications to learn about our offerings</p>
-            </div>
-                 <div className="mx-auto max-w-2xl py-10 px-4 sm:px-6 lg:max-w-7xl">
-                  <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                    <a key={product.id} href={product.href} className="group">
-                          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                            <img src={product.imageSrc} alt={product.imageAlt} className="mx-auto w-10/12 object-cover object-center group-hover:opacity-75"/>
-                          </div>
-                    <h3 className="mt-4 text-lg font-bold text-red-500">{product.name}</h3>
-                    <p className="mt-1 text-sm font-medium text-white">{product.type}</p>
-                    </a>
-                    ))}
+      <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto'>
+        <div className='pb-2'>
+            <p className='text-4xl font-bold inline border-b-4 border-red-500'>Menu</p>
+            <p className='py-6'>Click on our collectible NFT publications to learn about our offerings</p>
+        </div>
+        <div className="mx-auto max-w-2xl py-10 px-4 sm:px-6 lg:max-w-7xl">
+           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                    <img src={product.imageSrc} alt={product.imageAlt} className="mx-auto w-10/12 object-cover object-center group-hover:opacity-75"/>
                   </div>
-                 </div>
+              <h3 className="mt-4 text-lg font-bold text-red-500">{product.name}</h3>
+              <p className="mt-1 text-sm font-medium text-white">{product.type}</p>
+              </a>
+              ))}
+           </div>
+        </div>
       </div>
-</div>
-
-
-
-
-)
-}
+    </div>
+)}
      
