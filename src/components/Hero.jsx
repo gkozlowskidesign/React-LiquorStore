429eea0f3fd656bb180d6aa7a1c3704f3ad84691
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 00:37:44 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Example() {
         <div className="pt-20 mb-40">
           <div className="relative mx-auto max-w-7xl px-20 sm:static">
             <div className="sm:max-w-lg">
-              <h1 className="font text-4xl font-bold tracking-tight text-white sm:text-7xl">Cindy's <span className="text-red-600"> Wine</span></h1>
-              <h1 className="font text-4xl font-bold tracking-tight text-red-600 sm:text-7xl">And <span className="text-red-600">
+              <h1 className="font text-4xl font-bold tracking-tight text-white sm:text-7xl justify-center text-center">Cindy's <span className="text-red-600"> Wine</span></h1>
+              <h1 className="font text-4xl font-bold tracking-tight text-red-600 sm:text-7xl justify-center text-center">And <span className="text-red-600">
                   Spirits</span></h1>
             </div>
           </div>
