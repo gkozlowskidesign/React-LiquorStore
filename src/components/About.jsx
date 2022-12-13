21de047894f91ad5fa1166b82fd32748f1b29a4e
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 00:47:28 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
            <div className="grid grid-cols-1 grid-rows-1 gap-2 mb-10">
                   <div className='pb-2'>
-                    <p className='text-4xl text-white font-bold inline border-b-4 border-red-500'>About</p>
+                    <p className='text-4xl text-white font-bold inline border-b-4 border-red-400'>About</p>
                     <p className='py-6 text-white'>Cindys Wine and Spirits offers an amazing beautiful shopping experience. The store is filled with a huge selection of wines from all over the world. 
                     We have walls of vodka, Bourbons, Tequilas, Rums, Gins, Whiskeys, Moonshines and other liquor we also deliver gift baskets locally and have over 100 
                     cold wines and we also have a beer cave with craft Brews, Domestic and Imported beers. Our selection is by far the best on the Connecticut shoreline.</p>
                   </div>
                   <div className='pb-2 mt-4'>
-                    <p className='text-4xl text-white font-bold inline border-b-4 border-red-500'>History</p>
+                    <p className='text-4xl text-white font-bold inline border-b-4 border-red-400'>History</p>
                     <p className='py-6 text-white'>On September 8th 2022 we closed our doors at 688 Boston Post Rd, Westbrook Connecticut and moved to our newly built store location at 1238 Boston Post Rd, Westbrook Connecticut. 
                     Our new location opened its doors on September 13th 2022 in the heart of Westbrook right down the street from the green and in our beautiful new shop.</p>
                   </div>
