c73a7381acda1a847fe4bc445fe5022d367e8c92
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 11:22:44 -0500

    Update Shopping.jsx

--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -1,1 +1,23 @@
 
+import React from 'react'
+
+
+const Beer = () => {
+  return (
+
+    
+  <div>
+
+     <div className='w-full h-100 p-2 pb-10 mx-auto bg-white'>
+            <section className='mt-2 flex flex-col justify-center gap-2'>
+                  <div className="flex flex-wrap">
+                    <iframe className='mb-2' src="https://drizly.com/cindys-wine-and-spirits/s4712" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
+                    </div>
+            </section>
+    </div>
+</div>
+      
+  )
+}
+
+export default Beer
