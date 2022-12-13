dab1cefaccbcd620da3850a0b66f40a66b560d47
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-12 23:02:50 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Example() {
     return (
       <div id="Home" className="relative overflow-hidden w-full py-2 px-2 bg-gradient-to-b from-black to-red-400 text-white">
-        <div className="pt-10 mb-30">
+        <div className="pt-20 mb-20">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-8 lg:px-10">
             <div className="sm:max-w-lg">
               <h1 className="font text-4xl font-bold tracking-tight text-white sm:text-7xl">Cindy's <span className="text-red-600"> Wine</span></h1>
