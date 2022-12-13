c8016c96eef686ea3779581fe9aa780399f94cf8
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-12 23:44:27 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,36 +28,36 @@ const [nav, setNav] = useState(false)
         <nav>
             <ul className='flex flex-col p-14 text-gray-800'>
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiHomeAlt size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="/" className="mt-2 text-red-500 hover:text-red-300">Home</a></li>
+                      <BiHomeAlt size={30} className='mr-4  text-red-400' />
+                    <a href="/" className="mt-2 text-red-300 hover:text-red-200">Home</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiShoppingBag size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="/order" className="mt-2 text-red-500 hover:text-red-300">Order</a></li>
+                      <BiShoppingBag size={30} className='mr-4  text-red-400' />
+                    <a href="/order" className="mt-2 text-red-300 hover:text-red-200">Order</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiNews size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="#Learn-More" className="mt-2 text-red-500 hover:text-red-300">About</a></li>
+                      <BiNews size={30} className='mr-4  text-red-400' />
+                    <a href="#Learn-More" className="mt-2 text-red-300 hover:text-red-200">About</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiDrink size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="#Learn-More" className="mt-2 text-red-500 hover:text-red-300">Selection</a></li>
+                      <BiDrink size={30} className='mr-4  text-red-400' />
+                    <a href="#Learn-More" className="mt-2 text-red-300 hover:text-red-200">Selection</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiUserVoice size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="#Learn-More" className="mt-2 text-red-500 hover:text-red-300">Feedback</a></li>
+                      <BiUserVoice size={30} className='mr-4  text-red-400' />
+                    <a href="#Learn-More" className="mt-2 text-red-300 hover:text-red-200">Feedback</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiCommentCheck size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="https://linktr.ee/cindyswineandspirits" className="mt-2 text-red-500 hover:text-red-300">Connect</a></li>
+                      <BiCommentCheck size={30} className='mr-4  text-red-400' />
+                    <a href="https://linktr.ee/cindyswineandspirits" className="mt-2 text-red-300 hover:text-red-200">Connect</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiMap size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="https://goo.gl/maps/8Lpo34KA7kgbJgTj9" className="mt-2 text-red-500 hover:text-red-300">Location</a></li>
+                      <BiMap size={30} className='mr-4  text-red-400' />
+                    <a href="https://goo.gl/maps/8Lpo34KA7kgbJgTj9" className="mt-2 text-red-300 hover:text-red-300">Location</a></li>
                       
                     <li className='text-1xl py-4 pb-5 flex text-white'>
-                      <BiSupport size={30} className='mr-4  text-red-500 hover:text-red-300' />
-                    <a href="tel:(860) 399-0007" className="mt-2 text-red-500 hover:text-red-300">Call Us</a></li>
+                      <BiSupport size={30} className='mr-4  text-red-400' />
+                    <a href="tel:(860) 399-0007" className="mt-2 text-red-400 hover:text-red-300">Call Us</a></li>
 
             </ul>
         </nav>
