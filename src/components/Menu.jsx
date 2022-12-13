1b63f1ed6311d0e275cd99ba8ed11eb93baf8bfc
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 01:11:08 -0500

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -46,7 +46,7 @@ return (
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-xl ring-4 ring-black bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                     <img src={product.imageSrc} alt={product.imageAlt} className="mx-auto w-full object-cover object-center group-hover:opacity-75"/>
                   </div>
-              <h3 className="mt-4 text-lg font-bold text-red-500">{product.name}</h3>
+              <h3 className="mt-4 text-lg font-bold text-red-400">{product.name}</h3>
               <p className="mt-1 text-sm font-medium text-white">{product.type}</p>
               </a>
               ))}
