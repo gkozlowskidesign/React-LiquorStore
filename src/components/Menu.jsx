48082a2fad875131d84f5953669db500e83a3398
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 00:09:41 -0500

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -41,20 +41,20 @@ const products = [
                 <p className='text-4xl font-bold inline border-b-4 border-red-500'>Menu</p>
                 <p className='py-6'>Click on a publication to here about our offerings</p>
             </div>
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {products.map((product) => (
-        <a key={product.id} href={product.href} className="group">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-          <img src={product.imageSrc} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75"/>
-        </div>
-        <h3 className="mt-4 text-lg font-bold text-indigo-400">{product.name}</h3>
-        <p className="mt-1 text-sm font-medium text-white">{product.type}</p>
-        </a>
-        ))}
-        
+                 <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                  <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                    {products.map((product) => (
+                    <a key={product.id} href={product.href} className="group">
+                          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                            <img src={product.imageSrc} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                          </div>
+                    <h3 className="mt-4 text-lg font-bold text-indigo-400">{product.name}</h3>
+                    <p className="mt-1 text-sm font-medium text-white">{product.type}</p>
+                    </a>
+                    ))}
+                  </div>
+                 </div>
       </div>
-    </div>
 </div>
 
 
