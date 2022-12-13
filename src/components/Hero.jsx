832cb881994f8d689f26b007923f2259d09698fd
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-12 22:59:56 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,12 @@
 export default function Example() {
     return (
       <div id="Home" className="relative overflow-hidden w-full py-2 px-2 bg-gradient-to-b from-black to-red-400 text-white">
-        <div className="pt-10 mb-20">
+        <div className="pt-10 mb-30">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-8 lg:px-10">
             <div className="sm:max-w-lg">
-              <h1 className="font text-5xl font-bold tracking-tight text-white sm:text-7xl">Cindy's <span className="text-red-600"> Wine</span></h1>
-              <h1 className="font text-5xl font-bold tracking-tight text-red-600 sm:text-7xl">And <span className="text-red-600">
+              <h1 className="font text-4xl font-bold tracking-tight text-white sm:text-7xl">Cindy's <span className="text-red-600"> Wine</span></h1>
+              <h1 className="font text-4xl font-bold tracking-tight text-red-600 sm:text-7xl">And <span className="text-red-600">
                   Spirits</span></h1>
-              <p className="mt-6 text-2xl text-white">
-              Voted #1 Best Wine 
-              and Liquor store on the Connecticut Shore line!
-              </p>
             </div>
           </div>
         </div>
