b233897e71fceaa85f9f4ca473adedeba8fb9cb3
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-13 00:32:43 -0500

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -38,11 +38,12 @@ return (
             <p className='text-4xl font-bold inline border-b-4 border-red-500'>Menu</p>
             <p className='py-6'>Click on our collectible NFT publications to learn about our offerings</p>
         </div>
+      
         <div className="mx-auto max-w-2xl py-10 px-4 sm:px-6 lg:max-w-7xl">
            <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {products.map((product) => (
               <a key={product.id} href={product.href} className="group">
-                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-xl ring-4 ring-red-500 bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                     <img src={product.imageSrc} alt={product.imageAlt} className="mx-auto w-10/12 object-cover object-center group-hover:opacity-75"/>
                   </div>
               <h3 className="mt-4 text-lg font-bold text-red-500">{product.name}</h3>
